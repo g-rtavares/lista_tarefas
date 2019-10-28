1830f1e2a74c36f8448b04bce6f24aa0a7b4d65b
Lab2d <lab2d@admin.local>
2019-10-28 19:31:27 -0300

    Upload

---
+++ lib/views/task_dialog.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:lista_tarefas/models/task.dart';
+
+class TaskDialog extends StatefulWidget {
+  final Task task;
+  TaskDialog({this.task});
+  @override
+  _TaskDialogState createState() => _TaskDialogState();
+}
+
+class _TaskDialogState extends State<TaskDialog> {
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  final _priorityController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Task _currentTask = Task();
+  @override
+  void initState() {
+    super.initState();
+    if (widget.task != null) {
+      _currentTask = Task.fromMap(widget.task.toMap());
+    }
+    _titleController.text = _currentTask.title;
+    _descriptionController.text = _currentTask.description;
+    _priorityController.text = _currentTask.priority;
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.clear();
+    _descriptionController.clear();
+    _priorityController.clear();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(widget.task == null ? 'Nova tarefa' : 'Editar tarefas'),
+      content: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextFormField(
+              controller: _titleController,
+              decoration: InputDecoration(labelText: 'Título'),
+              autofocus: true,
+              validator: (text) {
+                return text.isEmpty ? "Insira o titulo!" : null;
+              },
+            ),
+            TextFormField(
+              controller: _descriptionController,
+              keyboardType: TextInputType.multiline,
+              maxLines: 3,
+              decoration: InputDecoration(labelText: 'Descrição'),
+              validator: (text) {
+                return text.isEmpty ? "Insira a descrição!" : null;
+              },
+            ),
+            TextFormField(
+              controller: _priorityController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Prioridade de 1 a 5'),
+              validator: (text) {
+                if (text.isEmpty || int.parse(text) < 1 || int.parse(text) > 5)
+                  return "Insira a prioridade ( 1 A 5)!";
+                else
+                  return null;
+              },
+            )
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Cancelar'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          child: Text('Salvar'),
+          onPressed: () {
+            if (_formKey.currentState.validate()) {
+              _currentTask.title = _titleController.value.text;
+              _currentTask.description = _descriptionController.text;
+              _currentTask.priority = _priorityController.text;
+              Navigator.of(context).pop(_currentTask);
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
